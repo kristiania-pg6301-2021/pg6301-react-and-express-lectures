b6f3af2620fca44fb6fb95e3d5e74ad452066866
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-09 00:34:20 +0100

    Add routing

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,4 +1,48 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>React Rocks</h1>, document.getElementById("app"));
+import {BrowserRouter} from "react-router-dom";
+import {Route, Switch} from "react-router";
+
+
+function HomePage() {
+    return <>
+        <h1>Welcome to the quiz</h1>
+        <button>Start new quiz</button>
+    </>
+}
+
+function QuizPage({quizFactory}) {
+    const quiz = quizFactory();
+    const {question, alternatives} = quiz;
+    return <>
+        <h1>Quiz</h1>
+        <div>Question: {question}</div>
+        {alternatives.map(a =>
+            <button>{a}</button>
+        )}
+    </>
+}
+
+function App() {
+    const quiz = {
+        question: "Are you happy?",
+        alternatives: ["yes", "no", "maybe"]
+    }
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/quiz">
+                <QuizPage quizFactory={() => quiz} />
+            </Route>
+            <Route exact path="/">
+                <HomePage />
+            </Route>
+            <Route>
+                Not found
+            </Route>
+        </Switch>
+    </BrowserRouter>;
+}
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
