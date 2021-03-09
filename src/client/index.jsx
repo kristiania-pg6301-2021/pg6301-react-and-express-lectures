10792d24b43cb928916794479b3609cd8b335002
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-09 22:12:44 +0100

    Add react router

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,7 +1,54 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import {BrowserRouter, Link} from "react-router-dom";
+import {Switch, Route} from "react-router";
 
-ReactDOM.render(<h1>Welcome to the book application</h1>, document.getElementById("app"));
+
+function HomePage() {
+    return <>
+        <h1>Welcome to my book application</h1>
+        <ul>
+            <li><Link to="/books">List books</Link></li>
+            <li><Link to="/create">Create new book</Link></li>
+        </ul>
+    </>;
+}
+
+function BookListPage() {
+    return <h1>All books:</h1>
+}
+
+function NewBookPage() {
+    return <h1>Add a new book</h1>;
+}
+
+function EditBookPage() {
+    return <h1>Edit book</h1>
+}
+
+function Application() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/create">
+                <NewBookPage/>
+            </Route>
+            <Route path="/edit">
+                <EditBookPage/>
+            </Route>
+            <Route path="/books">
+                <BookListPage/>
+            </Route>
+            <Route exact path="/">
+                <HomePage/>
+            </Route>
+            <Route>
+                <h1>Not found</h1>
+            </Route>
+        </Switch>
+    </BrowserRouter>
+}
+
+ReactDOM.render(<Application/>, document.getElementById("app"));
 
 
