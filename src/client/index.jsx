f41ce5cb00bb9f7320ab77b932a1e6304ef00409
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-10 16:46:16 +0100

    Add react-router to the application

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,5 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link} from "react-router-dom";
+import {Route, Switch} from "react-router";
 
 
-ReactDOM.render(<div>Hello world</div>, document.getElementById("app"));
+function Application() {
+    return <BrowserRouter>
+        <Switch>
+            <Route path={"/books"}>
+                <h1>List all the books</h1>
+            </Route>
+            <Route path={"/create"}>
+                <h1>Create a new book</h1>
+            </Route>
+            <Route path={"/edit"}>
+                <h1>Edit an existing book</h1>
+            </Route>
+            <Route exact path={"/"}>
+                <h1>Book application home page</h1>
+                <ul>
+                    <li><Link to={"/books"}>List books</Link></li>
+                    <li><Link to={"/create"}>Add a book</Link></li>
+                </ul>
+            </Route>
+            <Route>
+                Page not found
+            </Route>
+        </Switch>
+    </BrowserRouter>;
+}
+
+
+ReactDOM.render(<Application />, document.getElementById("root"));
