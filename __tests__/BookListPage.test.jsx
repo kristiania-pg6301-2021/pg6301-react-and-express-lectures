b66f87c59f5e335d2609205db58d18bf4bbb6f4c
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-13 23:00:40 +0100

    Get book from API - fix test

--- __tests__/BookListPage.test.jsx
+++ __tests__/BookListPage.test.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import TestRenderer from "react-test-renderer";
 import {act} from "react-dom/test-utils";
 import {BookListPage} from "../src/client/BookListPage";
+import {MemoryRouter} from "react-router";
 
 class MockBookApi {
     async listBooks() {
@@ -17,17 +18,17 @@ describe("book list view", () => {
     it("shows loading view", async () => {
         let view;
         await TestRenderer.act(async () => {
-            view = TestRenderer.create(<BookListPage bookApi={new MockBookApi()} />);
+            view = TestRenderer.create(<MemoryRouter><BookListPage bookApi={new MockBookApi()} /></MemoryRouter>);
         });
         expect(view.toJSON()).toMatchSnapshot();
-        expect(view.root.findByType("li").children[0]).toEqual("Lord of the dance");
+        expect(view.root.findByType("a").children[0]).toEqual("Lord of the dance");
     });
 
     it("renders on real DOM", async () => {
         const container = document.createElement("div");
         document.body.appendChild(container);
         await act(async () => {
-            ReactDOM.render(<BookListPage bookApi={new MockBookApi()} />, container)
+            ReactDOM.render(<MemoryRouter><BookListPage bookApi={new MockBookApi()} /></MemoryRouter>, container)
         });
 
         expect(container.innerHTML).toMatchSnapshot();
