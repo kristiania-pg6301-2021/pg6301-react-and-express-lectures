7e6ebcb7210490a12a171009d487692ebdc0cdbe
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-13 22:16:04 +0100

    Add assertions to tests

--- __tests__/LoadingView.test.jsx
+++ __tests__/LoadingView.test.jsx
@@ -9,13 +9,17 @@ describe("loading view", () => {
     it("shows loading view", () => {
         const view = TestRenderer.create(<LoadingView />);
         expect(view.toJSON()).toMatchSnapshot();
+        expect(view.root.findByType("div").children).toEqual(["Loading ..."]);
     });
     
     it("renders on real DOM", () => {
         const container = document.createElement("div");
         document.body.appendChild(container);
-        act(() => ReactDOM.render(<LoadingView />, container));
+        act(() => {
+            ReactDOM.render(<LoadingView />, container)
+        });
         
         expect(container.innerHTML).toMatchSnapshot();
+        expect(container.querySelector("div").textContent).toEqual("Loading ...");
     })
 });
