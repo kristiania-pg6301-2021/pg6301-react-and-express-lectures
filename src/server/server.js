aba4b5e0d4c8bc65547a243f9e593510f9d01b30
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-15 23:04:35 +0100

    Serve app from express

---
+++ src/server/server.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve(__dirname, "..", "..", "dist", "index.html"));
+  } else {
+    next();
+  }
+})
+
+
+app.listen(3000, () => {
+  console.log("App started");
+})
