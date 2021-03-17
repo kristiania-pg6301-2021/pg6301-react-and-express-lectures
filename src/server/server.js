90e60c41d039eda75a1a7b4d2cff0792e6245493
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-17 23:18:20 +0100

    Rudimentary CORS support from server

--- src/server/server.js
+++ src/server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/user", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:1234");
   res.json({ username: "Johannes from server" });
 });
 
