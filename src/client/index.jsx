8b0bb048ae9a32bc7347cbe9586a13efbd19a14b
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-03-24 12:35:39 +0100

    Route in frontend

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,4 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link} from "react-router-dom";
+import {Route, Switch} from "react-router";
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("app"));
+function ProfilePage() {
+    return <div>
+        <h1>Your profile:</h1>
+        <div>Username: Johannes</div>
+    </div>;
+}
+
+function LoginPage() {
+    return null;
+}
+
+function FrontPage() {
+    return <div>
+        <h1>Welcome</h1>
+        <ul>
+            <li><Link to="/profile">Profile page</Link></li>
+            <li><Link to="/login">Login</Link></li>
+        </ul>
+    </div>;
+}
+
+function Application() {
+    return <BrowserRouter>
+        <header>
+            <Link to="/">Front page</Link>
+        </header>
+        <Switch>
+            <Route path="/profile">
+                <ProfilePage />
+            </Route>
+            <Route path="/login">
+                <LoginPage />
+            </Route>
+            <Route exact path="/">
+                <FrontPage />
+            </Route>
+            <Route>
+                <h1>Not found</h1>
+            </Route>
+        </Switch>
+    </BrowserRouter>;
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- src/server/server.js
+++ src/server/server.js
@@ -8,6 +8,13 @@ const app = express();
 console.log("Hello world");
 
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.resolve(__dirname, "..", "..", "dist", "index.html"));
+    } else {
+        next();
+    }
+})
 
 const server = https.createServer({
     key: fs.readFileSync("server.key"),
