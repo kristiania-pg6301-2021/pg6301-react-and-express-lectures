a9089678058fdb5d80cb5a9b678428aada9b3682
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-04 19:54:52 +0200

    Frontend login with Google (remove passport)

--- src/server/server.js
+++ src/server/server.js
@@ -4,64 +4,11 @@ const path = require("path");
 const https = require("https");
 const fs = require("fs");
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const session = require("express-session");
-const passport = require("passport");
 const fetch = require("node-fetch");
-const LocalStrategy = require("passport-local").Strategy;
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
 
 const app = express();
 
-app.use(
-  session({
-    secret: "32bJS7s5k5al",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
 app.use(bodyParser.json());
-app.use(cookieParser());
-
-passport.use(
-  new LocalStrategy((username, password, done) => {
-    if (username === "johannes" && password === "secret123") {
-      done(null, { username, is_admin: true });
-    } else {
-      done(null, false, { message: "Invalid username/password" });
-    }
-  })
-);
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/oauth2callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log(profile);
-      done(null, { username: profile.displayName });
-    }
-  )
-);
-
-app.use(passport.initialize());
-app.use(passport.session());
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((id, done) => done(null, id));
-
-app.get("/api/profile", (req, res) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://webapps.kristiania.no:1234"
-  );
-  if (!req.user) {
-    return res.status(401).send();
-  }
-  const { username } = req.user;
-  res.json({ username });
-});
 
 app.post("/api/profile", async (req, res) => {
   res.header(
@@ -98,15 +45,6 @@ app.options("/api/profile", (req, res) => {
   );
   res.header("Access-Control-Allow-Headers", "*");
   res.end();
-});
-
-app.post("/api/login", passport.authenticate("google"), (req, res) => {
-  res.end();
-});
-
-app.get("/login", passport.authenticate("google", { scope: ["profile"] }));
-app.get("/oauth2callback", passport.authenticate("google"), (req, res) => {
-  res.redirect("/");
 });
 
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
