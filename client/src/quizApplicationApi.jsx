2625f3c49b587261f1a04bb8597d9a686b9e57b1
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-05 00:55:59 +0200

    Load login-provider from the server

--- client/src/quizApplicationApi.jsx
+++ client/src/quizApplicationApi.jsx
@@ -1,17 +1,19 @@
+class HttpError extends Error {
+  constructor(url, status, statusText) {
+    super(`${url}: ${status} ${statusText}`);
+  }
+}
+
+async function fetchJSON(url) {
+  let response = await fetch(url);
+  if (!response.ok) {
+    throw new HttpError(url, response.status, response.statusText);
+  }
+  return await response.json();
+}
+
 export class QuizApplicationApi {
-  getUserinfo() {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (Math.random() < 0.25) {
-          return reject("IO error");
-        }
-        resolve({
-          user: {
-            username: "Hello",
-            identifier: "hello@example.com",
-          },
-        });
-      }, 200);
-    });
+  async getUserinfo() {
+    return await fetchJSON("https://webapps.kristiania.no:3000/api/userinfo");
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const https = require("https");
 const fs = require("fs");
 
 const app = express();
+app.use(cors());
+
+app.get("/api/userinfo", (req, res) => {
+  res.json({
+    loginProvider: {
+      discovery_url:
+        "https://login.microsoftonline.com/common/.well-known/openid-configuration",
+      client_id: process.env.MICROSOFT_CLIENT_ID,
+    },
+  });
+});
 
 const server = https
   .createServer(
