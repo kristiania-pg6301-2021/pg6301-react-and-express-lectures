ad19c31200a0965fdd5afe14309a1fe41894ed1b
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-07 15:03:36 +0200

    Call API from React

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,4 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+function useLoader(loadingFunction) {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  
+  useEffect(async() => {
+    setLoading(true);
+    setData(undefined);
+    setError(undefined);
+    try {
+      setData(await loadingFunction());
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+  
+  return {data, loading, error};
+}
+
+function ErrorView({error}) {
+  return <div>Something went wrong: {error.toString()}</div>;
+}
+
+function LoadingView() {
+  return <div>Loading...</div>;
+}
+
+function Application({loadProfile}) {
+  const {data, loading, error} = useLoader(() => loadProfile());
+  
+  if (error) {
+    return <ErrorView error={error} />;
+  }
+  if (loading || !data) {
+    return <LoadingView />;
+  }
+  
+  return <h1>Hello world</h1>;
+}
+
+async function fetchJSON(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`${url}: ${res.status} ${res.statusText}`);
+  }
+  return await res.json();
+}
+
+async function loadProfile() {
+  return await fetchJSON("http://localhost:3000/api/profile");
+}
+
+ReactDOM.render(<Application loadProfile={loadProfile} />, document.getElementById("app"));
--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 app.get("/api/profile", (req, res) => {
   res.json({
-    hello: "world"
+    hello: "world",
+    loginProvider: {
+      discoveryUrl: "https://accounts.google.com/.well-known/openid-configuration",
+      client_id: "89654971890-sc10avhkormbba0dcii3uu66n3bg00ks.apps.googleusercontent.com"
+    }
   })
 })
 
