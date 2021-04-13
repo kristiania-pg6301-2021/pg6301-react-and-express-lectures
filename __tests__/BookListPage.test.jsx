5c8fd51bed72efc97fb3c525f7f8c901f44616f4
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-13 02:10:11 +0200

    Update BookListPage.test.jsx

--- __tests__/BookListPage.test.jsx
+++ __tests__/BookListPage.test.jsx
@@ -1,35 +1,46 @@
-import { LoadingView } from "../src/client/LoadingView";
-import TestRenderer from "react-test-renderer";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
 import { BookListPage } from "../src/client/BookListPage";
+import { MemoryRouter } from "react-router";
 
 const bookApi = {
   listBooks: async () => [{ id: 1, title: "Hakkebakkeskogen" }],
 };
 
-describe("book list page", () => {
-  it("show books", async () => {
-    let view;
-    await TestRenderer.act(async () => {
-      view = TestRenderer.create(<BookListPage bookApi={bookApi} />);
-    });
-    expect(view.toJSON()).toMatchSnapshot();
-    expect(view.root.findByType("li").children[0]).toEqual("Hakkebakkeskogen");
+async function render(component) {
+  const container = document.createElement("div");
+  document.body.appendChild(container);
+  await act(async () => {
+    ReactDOM.render(<MemoryRouter>{component}</MemoryRouter>, container);
   });
+  return container;
+}
 
+describe("book list page", () => {
   it("show books on dom", async () => {
-    const container = document.createElement("div");
-    document.body.appendChild(container);
-    await act(async () => {
-      ReactDOM.render(<BookListPage bookApi={bookApi} />, container);
-    });
-
+    const container = await render(<BookListPage bookApi={bookApi} />);
     expect(container.innerHTML).toMatchSnapshot();
     expect(container.querySelector("li").textContent).toEqual(
       "Hakkebakkeskogen"
+    );
+  });
+
+  it("shows book loading page", async () => {
+    const listBooks = () => new Promise(() => {});
+    const container = await render(<BookListPage bookApi={{ listBooks }} />);
+    expect(container.innerHTML).toMatchSnapshot();
+    expect(container.querySelector("div").textContent).toEqual("Loading ...");
+  });
+
+  it("shows book error page", async () => {
+    const listBooks = async () => {
+      throw Error("I don't like this");
+    };
+    const container = await render(<BookListPage bookApi={{ listBooks }} />);
+    expect(container.innerHTML).toMatchSnapshot();
+    expect(container.querySelector("div").textContent).toEqual(
+      "Something went wrong: Error: I don't like this"
     );
   });
 });
