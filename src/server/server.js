cd5a86b0892cc65898324376ecf1d08dfb8d8d50
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-14 14:04:24 +0200

    Introduce bookApi router to put all book api functions together

--- src/server/server.js
+++ src/server/server.js
@@ -22,18 +22,20 @@ const books = [
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 
-app.get("/api/books", (req, res) => {
+const bookApi = express.Router();
+
+bookApi.get("", (req, res) => {
   console.log(books);
   res.json(books);
 });
 
-app.get("/api/books/:id", (req, res) => {
+bookApi.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const book = books.find((b) => b.id === id);
   res.json(book);
 });
 
-app.put("/api/books/:id", (req, res) => {
+bookApi.put("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const bookIndex = books.findIndex((b) => b.id === id);
   const { title, author, year } = req.body;
@@ -41,12 +43,14 @@ app.put("/api/books/:id", (req, res) => {
   res.status(200).end();
 });
 
-app.post("/api/books", (req, res) => {
+bookApi.post("", (req, res) => {
   const { title, author, year } = req.body;
   console.log(req.body);
   books.push({ title, author, year, id: books.length + 1 });
   res.status(201).end();
 });
+
+app.use("/api/books", bookApi);
 
 app.use((req, res, next) => {
   if (req.method !== "GET" || req.path.startsWith("/api")) {
