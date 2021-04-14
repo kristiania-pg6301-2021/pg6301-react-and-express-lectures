c01041ea4795bc9e37edfeb58ca8eb652c764e42
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-14 10:58:51 +0200

    Put /api/books in a separate router - extract booksRouter to separate file

--- src/server/server.js
+++ src/server/server.js
@@ -1,56 +1,13 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const booksRouter = require("./booksRouter");
 
 const app = express();
 
-const books = [
-  {
-    id: 1,
-    title: "Uncle Tom's Cabin",
-    author: "Harriet Stowe",
-    year: "1852",
-  },
-  {
-    id: 2,
-    title: "Hakkebakkeskogen",
-    author: "Thorbjørn Egner",
-    year: "1953",
-  },
-];
-
 app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
-
-const router = express.Router();
-
-router.get("/", (req, res) => {
-  console.log(books);
-  res.json(books);
-});
-
-router.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const book = books.find((b) => b.id === id);
-  res.json(book);
-});
-
-router.put("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const bookIndex = books.findIndex((b) => b.id === id);
-  const { title, author, year } = req.body;
-  books[bookIndex] = { title, author, year, id };
-  res.status(200).end();
-});
-
-router.post("/", (req, res) => {
-  const { title, author, year } = req.body;
-  console.log(req.body);
-  books.push({ title, author, year, id: books.length + 1 });
-  res.status(201).end();
-});
-
-app.use("/api/books", router);
+app.use("/api/books", booksRouter);
 
 app.use((req, res, next) => {
   if (req.method !== "GET" || req.path.startsWith("/api")) {
