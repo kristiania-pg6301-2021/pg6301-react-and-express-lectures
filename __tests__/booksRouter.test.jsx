0d9e6f5a7532f8c1c2c2e1570de8cc3e6f581760
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-14 11:14:10 +0200

    Failing post test (because of missing test setup)

--- __tests__/booksRouter.test.jsx
+++ __tests__/booksRouter.test.jsx
@@ -5,13 +5,31 @@ const app = express();
 app.use(require("../src/server/booksApi"));
 
 describe("books api", () => {
-  it("lists initial books", async () => {
+  it("can list initial books", async () => {
     await request(app)
       .get("/")
       .then((response) => {
         expect(response.body.find(({ id }) => id === 2)).toMatchObject({
           title: "Hakkebakkeskogen",
         });
+      });
+  });
+
+  it("can add a new book", async () => {
+    await request(app)
+      .post("/")
+      .send({
+        title: "My New Book",
+        author: "Tester",
+        year: 2021,
+      })
+      .expect(200);
+    await request(app)
+      .get("/")
+      .then((response) => {
+        expect(response.body.map(({ title }) => title)).toContain(
+          "My new book"
+        );
       });
   });
 });
