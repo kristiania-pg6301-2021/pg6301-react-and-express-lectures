9d35aa8c3dfd6ef12c9464f8c2ad8f631cfaf800
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-14 14:20:43 +0200

    supertest of API POST /api/books
    resolves #21

--- __tests__/bookApi.test.js
+++ __tests__/bookApi.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const express = require("express");
 
 const app = express();
+app.use(require("body-parser").json());
 app.use(require("../src/server/bookApi"));
 
 describe("book API", () => {
@@ -13,6 +14,22 @@ describe("book API", () => {
         expect(response.body.find(({ id }) => id === 2)).toMatchObject({
           title: "Hakkebakkeskogen",
         });
+      });
+  });
+
+  it("can create a new book", async () => {
+    await request(app)
+      .post("")
+      .send({
+        title: "Idioten",
+        author: "Dostoevsky",
+        year: "1868",
+      })
+      .expect(201);
+    await request(app)
+      .get("")
+      .then((response) => {
+        expect(response.body.map(({ title }) => title)).toContain("Idioten");
       });
   });
 });
