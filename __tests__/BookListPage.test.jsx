9d801d3d8d671f4b6a2df4af43ffc419cfb4538b
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-14 12:50:21 +0200

    Fix tests to use react-dom/test-util and include link

--- __tests__/BookListPage.test.jsx
+++ __tests__/BookListPage.test.jsx
@@ -1,30 +1,24 @@
-import { LoadingView } from "../src/client/LoadingView";
-import TestRenderer from "react-test-renderer";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
 import { BookListPage } from "../src/client/BookListPage";
+import { MemoryRouter } from "react-router";
 
 const bookApi = {
   listBooks: async () => [{ id: 1, title: "Hakkebakkeskogen" }],
 };
 
 describe("book list page", () => {
-  it("show books", async () => {
-    let view;
-    await TestRenderer.act(async () => {
-      view = TestRenderer.create(<BookListPage bookApi={bookApi} />);
-    });
-    expect(view.toJSON()).toMatchSnapshot();
-    expect(view.root.findByType("li").children[0]).toEqual("Hakkebakkeskogen");
-  });
-
   it("show books on dom", async () => {
     const container = document.createElement("div");
     document.body.appendChild(container);
     await act(async () => {
-      ReactDOM.render(<BookListPage bookApi={bookApi} />, container);
+      ReactDOM.render(
+        <MemoryRouter>
+          <BookListPage bookApi={bookApi} />
+        </MemoryRouter>,
+        container
+      );
     });
 
     expect(container.innerHTML).toMatchSnapshot();
--- __tests__/LoadingView.test.jsx
+++ __tests__/LoadingView.test.jsx
@@ -1,24 +1,16 @@
-import {LoadingView} from "../src/client/LoadingView";
-import TestRenderer from "react-test-renderer";
-
+import { LoadingView } from "../src/client/LoadingView";
 import React from "react";
 import ReactDOM from "react-dom";
-import  { act} from "react-dom/test-utils"
+import { act } from "react-dom/test-utils";
 
 describe("loading view", () => {
-    it("show loading view", () => {
-        const view = TestRenderer.create(<LoadingView />);
-        expect(view.toJSON()).toMatchSnapshot();
+  it("show loading view on dom", () => {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+    act(() => {
+      ReactDOM.render(<LoadingView />, container);
     });
-    
-    it("show loading view on dom", () => {
-        const container = document.createElement("div");
-        document.body.appendChild(container);
-        act(() => {
-            ReactDOM.render(<LoadingView />, container)
-        });
-        
-        expect(container.innerHTML).toMatchSnapshot();
-    })
-    
-})
+
+    expect(container.innerHTML).toMatchSnapshot();
+  });
+});
