799b5228c1318cfb21179feb31dad946c3fa73d4
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-21 16:14:47 +0200

    Web sockets in chat page

--- src/client/ChatPage.jsx
+++ src/client/ChatPage.jsx
@@ -1,13 +1,37 @@
 import * as React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function ChatPage() {
   const [chatLog, setChatLog] = useState([]);
   const [message, setMessage] = useState("");
+  const [ws, setWs] = useState();
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:3000");
+    ws.onmessage = (event) => {
+      console.log("message", event);
+      const { message, id } = JSON.parse(event.data);
+      setChatLog((chatLog) => [...chatLog, id + ": " + message]);
+    };
+    ws.onopen = (event) => {
+      ws.send(
+        JSON.stringify({
+          type: "login",
+          username: "johannes",
+        })
+      );
+    };
+    setWs(ws);
+  }, []);
 
   function handleSubmitMessage(e) {
     e.preventDefault();
-    setChatLog([...chatLog, message]);
+    ws.send(
+      JSON.stringify({
+        type: "message",
+        message: message,
+      })
+    );
     setMessage("");
   }
 
--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,27 @@ const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const app = express();
+const ws = require("ws");
+
+const wsServer = new ws.Server({ noServer: true });
+const sockets = [];
+let messageIndex = 0;
+wsServer.on("connection", (socket) => {
+  // Add this connection to the list of connections
+  sockets.push(socket);
+  // Set up the handling of messages from this sockets
+  socket.on("message", (msg) => {
+    const wsMessage = JSON.parse(msg);
+    const { type } = wsMessage;
+    if (type === "message") {
+      const { message } = wsMessage;
+      const id = messageIndex++;
+      for (const recipient of sockets) {
+        recipient.send(JSON.stringify({ id, message }));
+      }
+    }
+  });
+});
 
 app.use(cors());
 
@@ -17,4 +38,11 @@ app.use((req, res, next) => {
   return next();
 });
 
-app.listen(3000);
+const server = app.listen(3000, () => {
+  server.on("upgrade", (req, socket, head) => {
+    wsServer.handleUpgrade(req, socket, head, (socket) => {
+      // This will pass control to `wsServer.on("connection")`
+      wsServer.emit("connection", socket, req);
+    });
+  });
+});
