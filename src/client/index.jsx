7d41755f605190b50f58284e47b5ed710731c7f6
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-21 13:06:43 +0200

    Home alone chat - we can chat with ourselves

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,4 +1,45 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { useState } from "react";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("app"));
+function ChatApplication() {
+  const ws = new WebSocket();
+  ws.onmessage;
+
+  const [chatLog, setChatLog] = useState([]);
+  const [message, setMessage] = useState("");
+
+  function handleSubmitChatMessage(e) {
+    e.preventDefault();
+    setChatLog([...chatLog, message]);
+    setMessage("");
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Welcome to Home Alone Chat</h1>
+      </header>
+      <main>
+        <div id="chatLog">
+          {chatLog.map((message, index) => (
+            <div key={index}>{message}</div>
+          ))}
+        </div>
+      </main>
+      <footer>
+        <form onSubmit={handleSubmitChatMessage}>
+          <input
+            type="text"
+            autoFocus={true}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button>Submit</button>
+        </form>
+      </footer>
+    </>
+  );
+}
+
+ReactDOM.render(<ChatApplication />, document.getElementById("app"));
