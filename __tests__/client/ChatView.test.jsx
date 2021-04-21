40d3189adafb12d4fc56363867e6662c72c00946
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-21 14:14:07 +0200

    Submit test for ChatView

--- __tests__/client/ChatView.test.jsx
+++ __tests__/client/ChatView.test.jsx
@@ -1,6 +1,7 @@
 import { ChatView } from "../../src/client/ChatView";
 import * as ReactDOM from "react-dom";
 import * as React from "react";
+import { Simulate } from "react-dom/test-utils";
 
 describe("chat view", () => {
   it("shows chat log as messages", () => {
@@ -13,5 +14,21 @@ describe("chat view", () => {
     expect(container.querySelector("#chatLog div").textContent).toEqual(
       "hello"
     );
+  });
+
+  it("submits a new chat message", () => {
+    const container = document.createElement("div");
+    const onSendMessage = jest.fn();
+    ReactDOM.render(
+      <ChatView chatLog={[]} onSendMessage={onSendMessage} />,
+      container
+    );
+
+    Simulate.change(container.querySelector("input"), {
+      target: { value: "Hello World" },
+    });
+    Simulate.submit(container.querySelector("form"));
+
+    expect(onSendMessage).toBeCalledWith("Hello World");
   });
 });
