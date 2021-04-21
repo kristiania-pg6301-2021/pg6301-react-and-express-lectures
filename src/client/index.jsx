1c7a3afb59bf20995d4be34572b427ab6225e0ee
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2021-04-21 15:07:20 +0200

    Routing and four pages!

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,4 +1,41 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { BrowserRouter, Link } from "react-router-dom";
+import { Route, Switch } from "react-router";
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById("app"));
+function Application() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link to={"/"}>Home</Link>
+      </header>
+      <Switch>
+        <Route path={"/"} exact>
+          <h1>Home page</h1>
+          <ul>
+            <li>
+              <Link to={"/profile"}>Profile</Link>
+            </li>
+            <li>
+              <Link to={"/chat"}>Chat</Link>
+            </li>
+            <li>
+              <Link to={"/login"}>Login</Link>
+            </li>
+          </ul>
+        </Route>
+        <Route path={"/profile"}>
+          <h1>Profile page</h1>
+        </Route>
+        <Route path={"/chat"}>
+          <h1>Chat page</h1>
+        </Route>
+        <Route path={"/login"}>
+          <h1>Login page</h1>
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- src/server/server.js
+++ src/server/server.js
@@ -3,5 +3,13 @@ const path = require("path");
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    return res.sendFile(
+      path.resolve(__dirname, "..", "..", "dist", "index.html")
+    );
+  }
+  return next();
+});
 
 app.listen(3000);
